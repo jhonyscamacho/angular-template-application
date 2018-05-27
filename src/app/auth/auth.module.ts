0184c3bbf7fb5bd376c0c031912ca2813b63fed5
Jhonys Camacho <jhonys.camacho@gmail.com>
2018-05-27 11:52:04 -0300

    Fix error; Rename file.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
 // Application imports
-import { AuthRoutingModule } from './auth.routing.model';
+import { AuthRoutingModule } from './auth.routing.module';
 import { LoginComponent } from './login/login.component';
 import { SharedModule } from '../shared/shared.module';
 import { SignupComponent } from './signup/signup.component';
