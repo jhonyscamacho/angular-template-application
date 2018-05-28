4cd60dc03e7e36bda9a9d2e7a1b271d6c74f0415
Jonilson Jhonys Corrêa Camacho <jhonyscamacho@campos.rj.gov.br>
2018-05-28 13:34:06 -0300

    Splits up dashboard routes.

--- src/app/core/core.routing.module.ts
+++ src/app/core/core.routing.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // Aplication imports
+import { AuthGuard } from './../auth/auth.guard';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
-  { path: '', component: WelcomeComponent }
+  { path: '', component: WelcomeComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
